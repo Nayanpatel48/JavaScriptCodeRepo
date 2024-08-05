5cee9610304eb63452aacc2130ec1bafe8ed934f
nayan <www.xil187@gmail.com>
2024-08-05 16:36:50 +0530

    Datatypes learned

---
+++ Basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+//readability is important
+console.log(3+3)
+
+console.log("Nayan")
+
+let name = "Leo"
+let right = true
+let number = 89
+let state = null //null is an object
+
+//number => (2 to power 53 )-1
+//boolean
+//String
+//null => Stand alone value
+//undefined => 
+//symbol => unique
+
+//object
+console.log(typeof state)
+console.log(typeof name)
+confirm.log(typeof undefined)
---
+++ Basics/variable.js
@@ -0,0 +1,15 @@
+const accountId = 8989
+let accountEmail = "nayan67@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" // not a good use
+let acoountType
+
+//acoountId = 2 // not allowed
+
+console.log(accountId)
+
+/*
+prefer not to use var because of issue in block and functional scope
+*/
+
+console.table([accountEmail, accountId, accountPassword, acoountType])
