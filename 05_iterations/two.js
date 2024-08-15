e9cc6c07d0562f3b2004a8bd36c6923c161177aa
nayan <www.xil187@gmail.com>
2024-08-15 22:12:50 +0530

    while do while loop done

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+//always do investigative study cause it is the best
+// while (condition) {
+    
+// }
+//while loop syntax
+
+let i=1
+while(i<=10)
+{
+    console.log(i)
+    i+=2
+}
+
+const myArray = ["Flash", "Superhero", "Batman"]
+
+let j = 0
+while(myArray.length>j)
+{
+    console.log(`value of ${j} is ${myArray[j]}`)
+    j++
+}
+
+let key = 0;
+do 
+{
+    console.log(`key is ${key}`)
+    key++
+} while (key < 0);
+//key is 0
