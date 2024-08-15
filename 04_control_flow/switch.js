d635c1f62d07051dcc6bce13e748a902d6753687
nayan <www.xil187@gmail.com>
2024-08-15 19:11:28 +0530

    switch done

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,49 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+//switch case syntax
+
+const month = 3
+
+switch (month) 
+{
+    case 1:
+        console.log("January")  
+        break;
+    case 2:
+        console.log("February")  
+            break;
+    case 3:
+        console.log("March")  
+        break;
+    default:
+        console.log("Not matched")
+        break;
+}
+//march
+
+//what does break statement do?=>it breaks the control flow.
+
+const monthNew = "jan"
+
+switch (monthNew) 
+{
+    case "jan":
+        console.log("January")  
+        break;
+    case "feb":
+        console.log("February")  
+            break;
+    case "mar":
+        console.log("March")  
+        break;
+    default:
+        console.log("Not matched")
+        break;
+}
+//January
